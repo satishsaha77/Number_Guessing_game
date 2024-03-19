03fb57a33b00725febe2b6ac5d11a3581b5a977e
Satish Kumar Saha <127980364+satishsaha77@users.noreply.github.com>
2024-03-20 00:52:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,18 +94,3 @@ function newGame(){
         palyGame = true
     });
 }
-
-// Get the button element
-const button = document.getElementById('#newGame');
-
-// Add a mouseover event listener
-button.addEventListener('mouseover', () => {
-  // Change the button's background color
-  button.style.backgroundColor = 'blue';
-});
-
-// Add a mouseout event listener
-button.addEventListener('mouseout', () => {
-  // Change the button's background color back to its original color
-  button.style.backgroundColor = '';
-});
